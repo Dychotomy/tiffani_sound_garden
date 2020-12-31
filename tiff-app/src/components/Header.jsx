a15ed679a741a7dc69bad36220382f1a8d036d8a
dychotomy <dychotomy2000@yahoo.com>
2020-12-31 15:30:28 -0500

    basic header outline

--- tiff-app/src/components/Header.jsx
+++ tiff-app/src/components/Header.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+// import clouds from '../art_assets/IMG_7406.jpeg'
+import './header.css'
 
 const Header = () => {
     return (
         <div className="header">
-            <h1>
+            <h1 className='title'>
                 Tiffani Allen
             </h1>
+            <div className='nav'>Home About Music</div>
+            {/* <img className='header-background' src={clouds} alt='clouds' /> */}
         </div>
     )
 }
