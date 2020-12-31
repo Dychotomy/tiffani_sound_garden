5f736dbbfb682c81b523b97aa2413b0f741abacc
dychotomy <dychotomy2000@yahoo.com>
2020-12-31 16:24:15 -0500

    basic structure of the about section

--- tiff-app/src/components/Body.jsx
+++ tiff-app/src/components/Body.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import portrait from '../art_assets/10-08-2019-13-16-14.jpeg'
+import './body.css'
 
 const Body = () => {
     return (
-        <p>About section, add pictures?</p>
+        <div className='body'>
+            <img className='body-portrait' src={portrait} alt='Tiff' />
+            <p className='body-about'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus suscipit, praesentium, dignissimos dolore beatae quas sequi rem quidem, obcaecati reiciendis ipsum. Temporibus expedita totam omnis sit magnam earum consequuntur ipsam. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis voluptates harum veritatis nostrum natus doloribus laborum quis error illo laudantium, earum est magni eius consequuntur voluptatem, aliquam iste cumque quisquam!</p>
+        </div>
     )
 }
 
